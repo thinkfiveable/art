80d24d85c874f76dd787e26fdefaa1c7f1609a97
eilla1 <git@ella.cx>
2021-06-21 18:47:28 -0700

    feat: add fonts/colors & global styles

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,18 @@
 import Meta from "../components/Meta";
 
-
 export default function Home() {
   return (
- <div>
-   <Meta />
-   <main>
-     <section>
-       <h1>hello</h1>
-     </section>
-
-   </main>
- </div> 
-  )
+    <div className="bg-gray min-h-screen">
+      <Meta />
+      <main>
+        <section className="mx-8 md:mx-48 pt-24">
+          <h1 className="text-7xl font-rubik font-bold">Fiveable</h1>
+          <h2 className="font-palmer text-6xl">Art Collective</h2>
+          <p className="font-rubik text-xl font-light">
+            For creatives and artists to make and break.
+          </p>
+        </section>
+      </main>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        rubik: "Rubik, sans-serif",
+        palmer: "Palmer Lake Print",
+      },
+      colors: {
+        orange: "#F36D01",
+        black: "#171717",
+        gray: "#F0F1F3",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
